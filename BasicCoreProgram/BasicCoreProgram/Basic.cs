85e9f353587783c62d7592c0c2b6bbd59ee07fc0
Sourav2707 <smartsourav33@gmail.com>
2022-07-14 13:54:37 +0530

    [Sourav] - Leap year

--- BasicCoreProgram/BasicCoreProgram/Basic.cs
+++ BasicCoreProgram/BasicCoreProgram/Basic.cs
@@ -10,35 +10,25 @@ namespace BasicCoreProgram
     {
         public void Core()
         {
-            double head = 0, tail = 0;
-            double limit = 0.5;
-            Console.WriteLine("Enter the number of time the coin to be flipped");
-            int flip = Convert.ToInt32(Console.ReadLine());
-            if (flip > 0)
+            Console.WriteLine("Enter the 4 digit year you wish to check for leap year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int limit = 1000;
+            int leap = year % 4;
+            if (year >= limit)
             {
-                for (int i = 0; i < flip; i++)
+               if (leap == 0)
                 {
-                    Random random = new Random();
-                    double output = random.Next(0, 2);
-                    if (output > limit)
-                    {
-                        Console.WriteLine("Head");
-                        head += 1;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Tail");
-                        tail += 1;
-                    }
+                    Console.WriteLine($"{year} is a leap year");
                 }
-                double headPer = (head / flip) * 100;
-                double tailPer = (tail / flip) * 100;
-                Console.WriteLine($"The percentage of head is {headPer}");
-                Console.WriteLine($"The percentage of tail is {tailPer}");
+               else
+                {
+                    Console.WriteLine($"{year} is not a leap year");
+                }
+
             }
             else
             { 
-                Console.WriteLine("Invalid input only positive integers are allowed");
+                Console.WriteLine("Invalid input only 4 digit years are allowed");
             }
         }
     }
