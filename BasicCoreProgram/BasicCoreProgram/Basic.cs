851ce50b9de82068b3b45d2897429f25b523d2c8
Sourav2707 <smartsourav33@gmail.com>
2022-07-14 13:45:57 +0530

    [Sourav] - coin flip

--- BasicCoreProgram/BasicCoreProgram/Basic.cs
+++ BasicCoreProgram/BasicCoreProgram/Basic.cs
@@ -14,25 +14,32 @@ namespace BasicCoreProgram
             double limit = 0.5;
             Console.WriteLine("Enter the number of time the coin to be flipped");
             int flip = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < flip; i++)
+            if (flip > 0)
             {
-                Random random = new Random();
-                double output = random.Next(0, 2);
-                if (output > limit)
+                for (int i = 0; i < flip; i++)
                 {
-                    Console.WriteLine("Head");
-                    head += 1;
+                    Random random = new Random();
+                    double output = random.Next(0, 2);
+                    if (output > limit)
+                    {
+                        Console.WriteLine("Head");
+                        head += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tail");
+                        tail += 1;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Tail");
-                    tail += 1;
-                }
+                double headPer = (head / flip) * 100;
+                double tailPer = (tail / flip) * 100;
+                Console.WriteLine($"The percentage of head is {headPer}");
+                Console.WriteLine($"The percentage of tail is {tailPer}");
             }
-            double headPer = (head / flip) * 100;
-            double tailPer = (tail / flip) * 100;
-            Console.WriteLine($"The percentage of head is {headPer}");
-            Console.WriteLine($"The percentage of tail is {tailPer}");
+            else
+            { 
+                Console.WriteLine("Invalid input only positive integers are allowed");
+            }
         }
     }
 }
